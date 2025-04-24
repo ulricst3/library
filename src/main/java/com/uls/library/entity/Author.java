a1a4d1d30d9a35e7aa5183c6d6c50696ba06363c
Stefan <ulricst3@students.zhaw.ch>
2025-04-24 20:51:41 +0200

    Change GenerationType to IDENTITY

--- src/main/java/com/uls/library/entity/Author.java
+++ src/main/java/com/uls/library/entity/Author.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class Author {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Integer publishedBooks;
